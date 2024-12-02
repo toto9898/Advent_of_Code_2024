a2a89db850780227d89d9fe9a9fc3c38f9480342
Todor Todorov <33911367+toto9898@users.noreply.github.com>
2024-12-02 15:13:26 +0200

    Day 1 - Patr 2

--- Day 1 - Historian Hysteria/Program.cs
+++ Day 1 - Historian Hysteria/Program.cs
@@ -1,23 +1,40 @@
-﻿List<Int32> locations1 = new List<Int32>();
-List<Int32> locations2 = new List<Int32>();
+﻿List<int> list1 = [];
+List<int> list2 = [];
+Dictionary<int, int> similarities = [];
 
 string[] lines = File.ReadAllLines("e:/Programming/Advent_of_Code_2024/Day 1 - Historian Hysteria/input.txt");
 
 foreach (string line in lines)
 {
-
     string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-    locations1.Add(int.Parse(parts[0].Trim()));
-    locations2.Add(int.Parse(parts[1].Trim()));
+    int location1 = int.Parse(parts[0].Trim());
+    int location2 = int.Parse(parts[1].Trim());
+    list1.Add(location1);
+    list2.Add(location2);
+    
+    if (similarities.ContainsKey(location2))
+    {
+        similarities[location2]++;
+    }
+    else
+    {
+        similarities.Add(location2, 1);
+    }
 }
 
-locations1.Sort();
-locations2.Sort();
+list1.Sort();
+list2.Sort();
 
 Int64 totalDistance = 0;
-for (int i = 0; i < locations1.Count; i++)
+Int64 similarityScore = 0;
+for (int i = 0; i < list1.Count; i++)
 {
-    totalDistance += Math.Abs(locations1[i] - locations2[i]);
+    totalDistance += Math.Abs(list1[i] - list2[i]);
+    if ( similarities.TryGetValue(list1[i], out int value))
+    {
+        similarityScore += list1[i] * value;
+    }
 }
 
-Console.WriteLine(totalDistance);
+Console.WriteLine(totalDistance); // Part 1 answer
+Console.WriteLine(similarityScore); // Part 2 answer
